77a2c25343432f1b7492c5b258ec326d07cf8842
Alex Loyko <alex.loyko96@gmail.com>
2017-05-03 16:10:48 -0400

    Fixed imperial measurements to show up in the departure row and changed the mta logo to beta

--- app/component/Distance.js
+++ app/component/Distance.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ComponentUsageExample from './ComponentUsageExample';
+import config from '../configurations/config.default';
+import { displayImperialDistance } from '../util/geo-utils';
 
 
 const round = (distance) => {
@@ -7,13 +9,29 @@ const round = (distance) => {
   return distance - (distance % 100);
 };
 
+// function displayImperialDistance(meters) {
+//   const feet = meters * 3.2808399;
+
+//   /* eslint-disable yoda */
+
+//   if (feet < 100) {
+//     return `${Math.round(feet / 10) * 10} ft`; // Tens of feet
+//   } else if (feet < 1000) {
+//     return `${Math.round(feet / 50) * 50} ft`; // fifty feet
+//   }
+//   return `${(Math.round(feet / 528)) / 10} mi`; // tenth of a mile
+// }
+
+
 const Distance = (props) => {
   let distance;
   let roundedDistance;
 
   if (props.distance) {
     roundedDistance = round(props.distance);
-    if (roundedDistance < 1000) {
+    if (config.imperial) {
+      distance = displayImperialDistance(props.distance);
+    } else if (roundedDistance < 1000) {
       distance = `${roundedDistance}m`;
     } else {
       distance = `${(roundedDistance / 1000).toFixed(1)}km`;
