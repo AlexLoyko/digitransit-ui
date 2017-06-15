150cffd8ef982da1def9e94dad1c7b2faabb8ee1
Alex Loyko <alex.loyko96@gmail.com>
2017-06-15 12:58:45 -0400

    fixed RouteView problem

--- app/component/map/Line.js
+++ app/component/map/Line.js
@@ -49,6 +49,11 @@ export default class Line extends React.Component {
   render() {
     const className = cx([this.props.mode, { thin: this.props.thin }]);
 
+    let filteredPoints;
+    if (this.props.geometry) {
+      filteredPoints = this.props.geometry.filter(point => point.lat != null && point.lot !== null);
+    }
+
     const lineConfig = this.context.config.map.line;
     let haloWeight = this.props.thin ? lineConfig.halo.thinWeight : lineConfig.halo.weight;
     let legWeight = this.props.thin ? lineConfig.leg.thinWeight : lineConfig.leg.weight;
@@ -63,7 +68,7 @@ export default class Line extends React.Component {
         <Polyline
           key="halo"
           ref={(el) => { this.halo = el; }}
-          positions={this.props.geometry.filter(point => point.lat !== null && point.lon !== null)}
+          positions={filteredPoints}
           className={`leg-halo ${className}`}
           weight={haloWeight}
           interactive={false}
@@ -71,7 +76,7 @@ export default class Line extends React.Component {
         <Polyline
           key="line"
           ref={(el) => { this.line = el; }}
-          positions={this.props.geometry.filter(point => point.lat !== null && point.lon !== null)}
+          positions={filteredPoints}
           className={`leg ${className}`}
           color={this.props.color ? this.props.color : 'currentColor'}
           weight={legWeight}
