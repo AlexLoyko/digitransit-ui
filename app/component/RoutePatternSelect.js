96a9a216dd46e8612da30fba479781e2f77725ed
Alex Loyko <alex.loyko96@gmail.com>
2017-10-03 16:17:00 -0400

    Added the filter for patterns on routepage

--- app/component/RoutePatternSelect.js
+++ app/component/RoutePatternSelect.js
@@ -2,16 +2,44 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Relay from 'react-relay';
 import cx from 'classnames';
+import _ from 'lodash';
 import Icon from './Icon';
 import ComponentUsageExample from './ComponentUsageExample';
 import { routePatterns as exampleRoutePatterns } from './ExampleData';
 
 function RoutePatternSelect(props) {
-  const options = props.route && props.route.patterns.map(pattern =>
-    (<option key={pattern.code} value={pattern.code}>
-      {pattern.stops[0].name} ➔ {pattern.headsign}
-    </option>));
+  // const options = props.route && props.route.patterns.map(pattern =>
+  //   (<option key={pattern.code} value={pattern.code}>
+  //     {pattern.stops[0].name} ➔ {pattern.trips.tripsForDate.tripHeadsign}
+  //   </option>));
 
+  let used = [];
+  let options = [];
+  const mostPopPatterns = {};
+
+  for (let i = 0; i < props.route.patterns.length; i++) {
+    let tripsOnThisPattern = props.route.patterns[i].tripsForDate;
+    if (tripsOnThisPattern && tripsOnThisPattern.length > 0) {
+      if (!_.includes(used, tripsOnThisPattern[0].tripHeadsign)) {
+        used.push(tripsOnThisPattern[0].tripHeadsign);
+        mostPopPatterns[tripsOnThisPattern[0].tripHeadsign] = props.route.patterns[i];
+      } else {
+         if (mostPopPatterns[tripsOnThisPattern[0].tripHeadsign].tripsForDate.length < tripsOnThisPattern.length) {
+           mostPopPatterns[tripsOnThisPattern[0].tripHeadsign] = props.route.patterns[i];
+         }
+      }
+    }
+  }
+
+  for (var headsign in mostPopPatterns) {
+    if (mostPopPatterns.hasOwnProperty(headsign)) {
+      options.push(
+        <option key={mostPopPatterns[headsign].code} value={mostPopPatterns[headsign].code}>
+          {mostPopPatterns[headsign].stops[0].name} ➔ {headsign}
+        </option>);
+    }
+  }
+  
   return (
     <div className={cx('route-pattern-select', props.className)}>
       <Icon img="icon-icon_arrow-dropdown" />
@@ -49,7 +77,9 @@ export default Relay.createContainer(RoutePatternSelect, {
       fragment on Route {
         patterns {
           code
-          headsign
+          tripsForDate(serviceDay: "20171002"){
+            tripHeadsign
+          }
           stops {
             name
           }
